a9717e16751ab046cbe3247af319666d1addd1ce
Sergiy Illichevskyy <illichso@gmail.com>
2015-09-26 21:57:22 +0200

    init commit

---
+++ src/main/java/com/illichso/Calculator.java
@@ -0,0 +1,11 @@
+package com.illichso;
+
+public class Calculator {
+
+    public int abs(int arg){
+        if(arg<0){
+            return -arg;
+        }
+        return arg;
+    }
+}
---
+++ src/main/java/com/illichso/Car.java
@@ -0,0 +1,4 @@
+package com.illichso;
+
+public class Car {
+}
